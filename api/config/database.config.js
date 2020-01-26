e66f3d1aedeedef32a8a0b5ac037adc2a8d75a0b
kessyy <kather946@gmail.com>
2020-01-26 19:47:48 +0300

    connecting database

---
+++ api/config/database.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: 'mongodb+srv://kessyy:244466666@netflix-04qpf.mongodb.net/test?retryWrites=true&w=majority'
+}
--- api/index.js
+++ api/index.js
@@ -1,11 +1,34 @@
-let express = require('express')
-// Initialize the app
-let app = express();
-// Setup server port
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 var port = process.env.PORT || 8080;
-// Send message for default URL
-app.get('/', (req, res) => res.send('Hello World with Express'));
-// Launch app to listen to specified port
+
+app.get('/', (req, res) => res.send('Welcome to Netflix'));
+
+
 app.listen(port, function () {
      console.log("Live at " + port);
 });
